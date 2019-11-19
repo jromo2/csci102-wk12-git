c21a6eb4128d365815aa7480326839bed4df4080
Jake Romo <you@example.com>
2019-11-18 20:17:24 -0700

    updated Week12-utility.py

---
+++ Week12-utility.py
@@ -0,0 +1,13 @@
+"""
+Jacob Romo
+CSCI 102 Section A
+Week 12 Part A
+"""
+
+################################################
+########   Function 1 : PrintOutput    #########
+################################################
+
+def PrintOutput(string):
+    print('OUTPUT ',string)
+    
